17f99585aaa842193179f321141ef4f84a84bcc8
alanwhosthen <alanzaaiter@gmail.com>
2025-11-12 21:28:52 +0200

    Update manager.js

--- core/manager.js
+++ core/manager.js
@@ -7,7 +7,7 @@ class BotManager {
     constructor() {
         this.bots = new Map(); 
     }
-    
+
     async initializeBots() {
         logger.info({ sessions: SESSION }, `Initializing all configured bots.`);
         await CustomAuthState.deleteGarbageSessions(SESSION);        
@@ -28,15 +28,6 @@ class BotManager {
             }
         }
     }
-    const fs = require('fs');
-const path = './plugins/'; // folder where your lyrics.js is
-
-fs.readdirSync(path).forEach(file => {
-    if (file.endsWith('.js')) {
-        require(`${path}${file}`);
-        console.log(`Plugin loaded: ${file}`);
-    }
-});
 
     getBot(sessionId) {
         return this.bots.get(sessionId);
@@ -96,6 +87,5 @@ fs.readdirSync(path).forEach(file => {
         }
     }
 }
-
 
 module.exports = { BotManager };
